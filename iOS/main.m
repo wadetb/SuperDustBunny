cc1124bfd3632b5ed96d0920b19a68342804cafb
Wade Brainerd <wadeb@wadeb.com>
2011-01-23 02:36:10 +0000

    Additional files for iOS version.

---
+++ iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperDustBunny
+//
+//  Created by Wade Brainerd on 1/20/11.
+//  Copyright 2011 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
